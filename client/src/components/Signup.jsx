5a0b00b6ca5b7d8f50c819ff327ea0c482fe3cef
Julian Lopez <julian12lopez@gmail.com>
2020-07-11 14:04:30 -0400

    components set up, log in page designed loosely

---
+++ client/src/components/Signup.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+export default class Signup extends Component{
+
+
+
+    render(){
+
+        return(
+            <div>
+                <h1>This is for a Sign Up</h1>
+            </div>
+        )
+
+    }
+
+}
--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,30 @@
-var passport = require("passport");
+var passport = require("passport")
+var LocalStrategy = require('passport-local').Strategy;
+
+
+var db = require("../schema");
+
+    //user will sign in with username, will edit once established
+    passport.use(new LocalStrategy(
+
+        //must define User
+        function(username, password, done) {
+          User.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+
+            if (!user) {
+              return done(null, false, { message: 'Incorrect username.' });
+            }
+
+            if (!user.validPassword(password)) {
+              return done(null, false, { message: 'Incorrect password.' });
+            }
+
+            return done(null, user);
+
+          });
+        }
+      
+));
+
+
