8998fb9374317f62d7bf9b7208f3b4d9a4d8c6df
Julian Lopez <julian12lopez@gmail.com>
2020-07-17 14:17:31 -0400

    passport no longer affects code, updated db.js, trouble with server code

--- database/db.js
+++ database/db.js
@@ -1,20 +1,21 @@
 const Sequelize = require("sequelize");
-const sequelize = require("sequelize");
 const db = {}
+//const realtorstore_db = require("../schema/realtorstore_db.sql")
 // eslint-disable-next-line no-const-assign
-Sequelize = require("realtorstore_db","root","",{
-    host: "localhost",
-    dialect: "mysql",
+var sequelize = new Sequelize('realtorstore_db', 'root', 'BeacHrD1213', {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+    // SQLite only
+    storage: 'path/to/database.sqlite'
+  });
 
 
-    pool:{
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-});
-db.sequelize = sequelize;
-db.Sequelize = Sequelize
+db.Sequelize = Sequelize;
 
-module.exports = db
+module.exports = Sequelize;
