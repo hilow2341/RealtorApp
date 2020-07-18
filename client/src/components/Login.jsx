cb886d73278b4a31b9b3a2212233dafb918d8bd5
thefreshprince98 <58113441+thefreshprince98@users.noreply.github.com>
2020-07-18 13:45:31 -0400

    log in page almost done

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -11,44 +11,54 @@ export default class Login extends Component{
 
         return(
 <div>
-                <div className="field">
-                    <label className="label">Username</label>
-                    <div className="control has-icons-left has-icons-right">
-                        {/* add value */}
-                    <input className="input is-success" type="text" placeholder="Text input" />
-                     <span className="icon is-small is-left">
-                        <i className="fas fa-user"/>
-                    </span>
-                    <span className="icon is-small is-right">
-                        <i className="fas fa-check"/>
-                    </span>
+<div>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Login - Free Bulma template</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+        {/* <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet"> */}
+        <link href="https://fonts.googleapis.com/css?family=Questrial&display=swap" rel="stylesheet" />
+        
+        <link rel="stylesheet" href="https://unpkg.com/bulma@0.8.2/css/bulma.min.css" />
+        <link rel="stylesheet" type="text/css" href="../css/login.css" />
+        <section className="hero is-success is-fullheight">
+          <div className="hero-body">
+            <div className="container has-text-centered">
+              <div className="column is-4 is-offset-4">
+                <h3 className="title has-text-black">Login</h3>
+                <hr className="login-hr" />
+               
+                <div className="box">
+                  <figure className="avatar">
+                    {/* img */}
+                  </figure>
+                  <form>
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-large" type="email" placeholder="Your Email" autofocus />
+                      </div>
                     </div>
-                        <p className="help is-success">This username is available</p>
+                    <div className="field">
+                      <div className="control">
+                        <input className="input is-large" type="password" placeholder="Your Password" />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <label className="checkbox">
+                        <input type="checkbox" />
+                        Remember me
+                      </label>
+                    </div>
+                    <button className="button is-block is-info is-large is-fullwidth">Login <i className="fa fa-sign-in" aria-hidden="true" /></button>
+                  </form>
                 </div>
-                <div className="field">
-                    <label className="label">Password</label>
-                    <div className="control has-icons-left has-icons-right">
-                        {/* add value */}
-                        <input className="input is-success" type="password" placeholder="Text input" />
-                    <span className="icon is-small is-left">
-                        <i className="fas fa-user"/>
-                    </span>
-                    <span className="icon is-small is-right">
-                        <i className="fas fa-check"/>
-                    </span>
-                    </div>
-                </div>
-                <div className="field is-grouped">
-                    <div className="control">
-                        <button className="button is-link">Submit</button>
-                    </div>
-                    <div className="control">
-                        <Link to="/">
-                        <button className="button is-link is-light">Cancel</button>
-                        </Link>
-                    </div>
-                </div>
-
+              
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
 </div>
         )
 
